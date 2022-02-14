b385ad89c4584046de66597289c0ac7e50ea7bea
Ivan Megias <ivan92.md@gmail.com>
2022-02-14 22:36:12 +0100

    feat: improve header design with back button

--- src/pages/People.js
+++ src/pages/People.js
@@ -6,12 +6,13 @@ import logo from '../resources/images/logo.svg';
 const NotFound = () => (
   <div className="App">
     <header className="App-header">
+      <Link to="/">
+        <p className="btn-back">{'<< Back'}</p>
+      </Link>
       <p>People</p>
     </header>
     <main>
-      <Link to="/">
-        <img src={logo} className="App-logo" alt="logo" />
-      </Link>
+      <img src={logo} className="App-logo" alt="logo" />
     </main>
   </div>
 );
